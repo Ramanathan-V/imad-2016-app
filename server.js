2421626df2bf15253b50bf8c214fa63edb9621d3
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-10-01 11:54:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+            title:'Article one | Ram',
+            heading: 'Article one',
+            date:'Sep 30, 2016',
+            content:` <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+                <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+                <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>`
+            
+        } ;
+function Createtemplate (data) {
+var title = data.title;
+var headig = data.heading;
+var date = data.date;
+var content = data.content;
+
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+            </p>
+           </div>
+        </div>
+</html>
+ `;
+ return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(CreateTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
@@ -30,6 +88,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
