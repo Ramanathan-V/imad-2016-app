d32ba1bfbdd8bee2a27439373f724d99d038db79
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-10-08 20:19:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button = document.getElementById('counter');
  
 
 button.onclick = function () {
-    count = count + 1;
+    counter = counter + 1;
     var span = document.getElementById('count');
-    span.innerHTML = count.toString();
+    span.innerHTML = counter;
 };
