247564e2d05485de7619565f3eaa9e666d934043
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-10-08 19:42:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,25 @@ var button = document.getElementById('counter');
  
 
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+    };
+    
+     //make the request
+     request.open('GET','http://ramanathan-v.imad.hasura-app.io/counter',true);
+     request.send(null);
+  
+    
 };
