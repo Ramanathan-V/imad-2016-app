f55d9fd3fe71787bf422727a5665b281cf20a246
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-10-09 09:12:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var name = [];
+var names = [];
 
 app.get('/submit-name/:name',function(req,res) {
     //Get the name from request
