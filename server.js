19c15bcf4c2ec2a62a529fa1b8fe24c3eb47e496
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-11-19 23:33:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require ('pg').Pool ;
-
-var config = {
-    user:'ramanathan-v',
-    database:'ramanathan-v',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password: process.env.DB_PASSWORD
-};
 
 
 
