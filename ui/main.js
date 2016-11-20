c194cd68ed084f5f87b356c85f2986e70016604e
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-11-20 16:23:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick = function () {
      var username = document.getElementById('username').value;
      var password = document.getElementById('password').value;
      console.log(username);
-     console.log(pasword);
+     console.log(password);
      request.open('POST','http://ramanathan-v.imad.hasura-app.io/login',true);
      request.setRequestHeader('Content-Type','application/json');
      request.send(JSON.stringify({username:username,password:password}));  
