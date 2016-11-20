656cc2d7dad301055a472dca712d7e733e740f7d
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-11-20 13:14:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require ('pg').Pool ;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user:'ramanathan-v',
@@ -13,49 +16,14 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
-var articles = {
-        'article-one' : {
-            title:'Article one | Ram',
-            heading: 'Article one',
-            date:'Sep 20, 2016',
-            content:` <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-                <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-                <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                   This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>`
-            
-                },
-                      
-        'article-two' : {
-            title:'Article two | Ram',
-            heading: 'Article two',
-            date:'Sep 25, 2016',
-            content:` <p>
-                     This is the content for my second article
-                     </p>`
-                },
-                
-        'article-three' : {
-            title:'Article three | Ram',
-            heading: 'Article three',
-            date:'Sep 30, 2016',
-            content:` <p>
-                      This is the content for my third article.
-                      </p>`
-                }
-                
-            };
+
+
 function createTemplate (data) {
 var title = data.title;
 var heading = data.heading;
@@ -97,6 +65,69 @@ app.get('/',function (req, res)
 {
  res.sendFile(path.join(__dirname,'ui','index.html'));
 });
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "ramanathan-v, "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
