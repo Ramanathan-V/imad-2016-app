24ad0fb2895aa10f8f9ea08febe722c9e4f51b72
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-11-20 19:51:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require ('pg').Pool ;
+var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
-
-
 
 var config = {
     user:'ramanathan-v',
@@ -17,6 +14,7 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
+
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
@@ -25,48 +23,55 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-
 function createTemplate (data) {
-var title = data.title;
-var heading = data.heading;
-var date = data.date;
-var content = data.content;
-
-var htmlTemplate=`
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-           ${heading}
-        </h3>
-        <div>
-            ${date.toDateString()}
-        </div>
-        <div>
-            ${content}
-            </p>
-           </div>
-        </div>
-</html>
- `;
- return htmlTemplate;
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
 
-app.get('/',function (req, res)
-{
- res.sendFile(path.join(__dirname,'ui','index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 function hash (input, salt) {
     // How do we create a hash?
@@ -81,11 +86,9 @@ app.get('/hash/:input', function(req, res) {
 });
 
 app.post('/create-user', function (req, res) {
-   // username, password
- 
+  
    var username = req.body.username;
    var password = req.body.password;
-  
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
@@ -96,8 +99,6 @@ app.post('/create-user', function (req, res) {
       }
    });
 });
-
-
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
@@ -132,21 +133,25 @@ app.post('/login', function (req, res) {
    });
 });
 
-
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       res.send('You are logged in: ' + req.session.auth.userId.toString());
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
    } else {
-   
-       res.send('You are not logged in');
+       res.status(400).send('You are not logged in');
    }
 });
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('Logged out');
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
-
 
 var pool = new Pool(config);
 
