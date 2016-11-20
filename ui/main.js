e52039cb96355a799f87a4c735fec2158fb5876e
Ramanathan Venkataramani <v.ram23@gmail.com>
2016-11-20 18:39:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ submit.onclick = function () {
                 alert('Logged in successfully');
             } else if (request.status === 403 ) {
                 alert ('Username/password is incorrect');
-            } else if (request.status === 500)  {
+            } else if (request.status === 500) {
                 alert ('Something went wrong on the server');
             }
           }
